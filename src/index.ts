151f028a769b300bd44379d0c472c315be5df4b4
mikhael-dantas <mikhael-dcs@outlook.com>
2022-03-16 11:32:47 -0300

    conecting and sync database with prisma

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
 const app = express()
 app.use(cors())
 app.use(express.json())
@@ -8,10 +12,21 @@ app.use(express.json())
 const port = 3000
 
 
-app.post('/', (req, res) => {
-   const hello = 'Hello World!'
+app.post('/users', async (req, res) => {
+   const user = await prisma.user.create({
+      data: {
+         name: req.body.name,
+         email: req.body.email,
+      }
+   })
 
-   res.send(hello)
+   res.send(user)
+})
+
+app.get('/users', async (req, res) => {
+   const users = await prisma.user.findMany()
+   
+   res.send(users)
 })
 
 app.listen(port, () => {
