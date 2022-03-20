04d297029d205b0725972c5850f12f5bb7316022
mikhael-dantas <mikhael-dcs@outlook.com>
2022-03-20 13:31:06 -0300

    removing useless code

--- src/modules/users/useCases/CreateUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/CreateUser/CreateUserUseCase.ts
@@ -3,7 +3,6 @@ import { ICreateUserDTO } from "../../DTOs/UsersDTOs";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 import { UsersRepository } from "../../repositories/UsersRepository";
 
-@injectable()
 class CreateUserUseCase {
    private readonly usersRepository: IUsersRepository;
    constructor() {
