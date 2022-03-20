1d77d4a338e40b70bb93bd860759c80338e0a3e1
mikhael-dantas <mikhael-dcs@outlook.com>
2022-03-20 11:23:08 -0300

    conecting and refactoring resolver->usecase->repository->db pipeline

--- src/modules/users/repositories/UsersRepository.ts
+++ src/modules/users/repositories/UsersRepository.ts
@@ -32,19 +32,4 @@ export class UsersRepository implements IUsersRepository {
       const users = await this.dbCli.user.findMany();
       return users
    }
-
-   // async findByEmail(email: string): Promise<User | undefined> {
-   //    return this.dbCli.user.findUnique(
-   //       {
-   //          where: {
-   //             email,
-   //          },
-   //       },
-   //    );
-   // }
-
-   // async findById(user_id: string): Promise<User | undefined> {
-   //    return this.repository.findOne(user_id);
-   // }
-
 }
--- src/modules/users/resolvers/UsersResolver.ts
+++ src/modules/users/resolvers/UsersResolver.ts
@@ -1,95 +1,29 @@
-import { Resolver, Query, Arg, Ctx, Mutation} from "type-graphql";
+import { Resolver, Query, Arg, Ctx, Mutation, InputType, Args} from "type-graphql";
 import { User } from "../typeDefs/UserTypeDef";
+
 import { CreateUserUserCase } from "../useCases/CreateUser/CreateUserUseCase";
 import { ListUsersUseCase } from "../useCases/ListUsers/ListUsersUseCase";
 
-interface Context {
-
-}
-
 @Resolver(User)
 class UserResolver {
-   @Query(returns => User)
-   async User(@Arg("id") id: string) {
-      return {
-         id: "adgas",
-         name: "mikhael",
-         email: "mikhael@mikhael.com",
-         password: "123456",
-         created_at: new Date(),
-         updated_at: new Date(),
-      }
+
+   @Mutation(returns => User)
+   createUser(
+      @Arg("name") name: string,
+      @Arg("email") email: string,
+      @Arg("password") password: string,
+   ): Promise<User> {
+      const createUserUseCase = new CreateUserUserCase();
+      return createUserUseCase.execute({name, email, password});
    }
 
    @Query(returns => [User])
-   // users(@Args() { skip, take }: usersArgs) {
+   // async users(@Arg("skip") skip: number, @Arg("take") take: number) {
    async users() {
       const listUsersUseCase = new ListUsersUseCase();
       const users = await listUsersUseCase.execute();
       return users
    }
-
-   // @Mutation(returns => User)
-   // createUser(
-   //    @Arg("createUserData") createUserData: { name: string, email: string, password: string },
-   // ): Promise<User> {
-   //    const createUserUseCase = container.resolve(CreateUserUserCase);
-   //    return createUserUseCase.execute(createUserData);
-   // }
 }
 
 export { UserResolver };
-
-// import { Resolver, Query} from "type-graphql";
-// import  { Post } from "./Post";
-
-// @Resolver(Post)
-// class PostResolver {
-//    // @Query(returns => Post)
-//    // async Post(@Arg("id") id: string) {
-//    //    const Post = await this.postservice.findById(id);
-//    //    if (Post === undefined) {
-//    //       throw new PostNotFoundError(id);
-//    //    }
-//    //    return Post;
-//    // }
-
-//    @Query(returns => [Post])
-//    // posts(@Args() { skip, take }: postsArgs) {
-//    posts() {
-//       // return this.postservice.findAll({ skip, take });
-//       return [
-//          {
-//             id: "1",
-//             title: "The Awakening",
-//             content: "A book about the awakening",
-//             published: false,
-//             created_at: new Date(),
-//             updated_at: new Date(),
-//             author_id: "tyhsndsy",
-//          },
-//       ];
-//    }
-
-//    // @Mutation(returns => Post)
-//    // @Authorized()
-//    // addPost(
-//    //    @Arg("newPostData") newPostData: NewPostInput,
-//    //    @Ctx("user") user: User,
-//    // ): Promise<Post> {
-//    //    return this.postservice.addNew({ data: newPostData, user });
-//    // }
-
-//    // @Mutation(returns => Boolean)
-//    // @Authorized(Roles.Admin)
-//    // async removePost(@Arg("id") id: string) {
-//    //    try {
-//    //       await this.postservice.removeById(id);
-//    //       return true;
-//    //    } catch {
-//    //       return false;
-//    //    }
-//    // }
-// }
-
-// export { PostResolver };
