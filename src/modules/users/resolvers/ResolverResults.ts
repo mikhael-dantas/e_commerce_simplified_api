14e38c4526813dc7b31be2e9ef25e4f89cfbd7d1
mikhael-dantas <mikhael-dcs@outlook.com>
2022-03-23 19:46:01 -0300

    organizing all structures in the pipeline

--- src/modules/users/resolvers/ResolverResults.ts
+++ src/modules/users/resolvers/ResolverResults.ts
@@ -1,9 +1,18 @@
 import { createUnionType } from "type-graphql";
 import { ExpiredTokenErrorTypeDef } from "../../../shared/errors/GraphqlErrorDefs/ExpiredTokenError";
+import { InvalidTokenErrorTypeDef } from "../../../shared/errors/GraphqlErrorDefs/InvalidTokenError";
 import { MissingTokenErrorTypeDef } from "../../../shared/errors/GraphqlErrorDefs/MissingTokenError";
 import { ResourceNotFoundErrorTypeDef } from "../../../shared/errors/GraphqlErrorDefs/ResourceNotFoundError";
 import { UnauthorizedTokenErrorTypeDef } from "../../../shared/errors/GraphqlErrorDefs/UnauthorizedTokenError";
+import { Profile } from "../../profiles/typeDefs/ProfileTypeDef";
 import { User } from "../typeDefs/UserTypeDef";
+
+export const CreateUserResults = createUnionType({
+   name: "CreateUserResults",
+   types: () => [
+      User,
+   ] as const, 
+})
 
 export const SearchUserResults = createUnionType({
    name: "SearchUserResults",
@@ -11,8 +20,31 @@ export const SearchUserResults = createUnionType({
       User,
       ExpiredTokenErrorTypeDef,
       MissingTokenErrorTypeDef,
+      InvalidTokenErrorTypeDef,
       UnauthorizedTokenErrorTypeDef,
       ResourceNotFoundErrorTypeDef
    ] as const, 
-   
+})
+
+export const SearchUsersResults = createUnionType({
+   name: "SearchUsersResults",
+   types: () => [
+      User,
+      ExpiredTokenErrorTypeDef,
+      MissingTokenErrorTypeDef,
+      InvalidTokenErrorTypeDef,
+      UnauthorizedTokenErrorTypeDef,
+      ResourceNotFoundErrorTypeDef
+   ] as const, 
+})
+
+export const SearchUserProfileResults = createUnionType({
+   name: "SearchUserResults",
+   types: () => [
+      Profile,
+      ExpiredTokenErrorTypeDef,
+      MissingTokenErrorTypeDef,
+      UnauthorizedTokenErrorTypeDef,
+      ResourceNotFoundErrorTypeDef
+   ] as const, 
 })
--- src/modules/users/resolvers/UsersResolver.ts
+++ src/modules/users/resolvers/UsersResolver.ts
@@ -1,9 +1,17 @@
-import { Resolver, Query, Arg, Mutation, Args, FieldResolver, Root } from "type-graphql";
+import { Resolver, Query, Arg, Mutation, Args, FieldResolver, Root, Ctx } from "type-graphql";
 import { container } from "tsyringe";
 
 import { User } from "../typeDefs/UserTypeDef";
 import { Profile } from "../../profiles/typeDefs/ProfileTypeDef";
 
+import { 
+   CreateUserResults,
+   SearchUserProfileResults,
+   SearchUserResults,
+   SearchUsersResults
+} from "./ResolverResults";
+
+import { IContext } from "../../../app";
 import { createUserInput, FieldsToSearchUser, usersArgs } from "../DTOs/UsersDTOs";
 
 import { CreateUserUseCase } from "../useCases/CreateUser/CreateUserUseCase";
@@ -11,40 +19,50 @@ import { ListUsersUseCase } from "../useCases/ListUsers/ListUsersUseCase";
 import { FindUserUseCase } from "../useCases/FindUser/FindUserUseCase";
 import { FindProfileByUserIdUseCase } from "../../profiles/useCases/FindProfileByUserId/FindProfileByUserIdUseCase";
 
+
 @Resolver(User)
 class UsersResolver {
 
-   @Mutation(returns => User)
-   async createUser( @Arg("data") { name, email, password }: createUserInput
-   ): Promise<User> {
+   @Mutation(returns => CreateUserResults)
+   async createUser(
+      @Arg("data") { name, email, password }: createUserInput
+   ): Promise<typeof CreateUserResults> {
       const createUserUseCase = container.resolve(CreateUserUseCase);
       return await  createUserUseCase.execute({name, email, password});
    }
 
-   @Query(returns => User)
+   @Query(returns => SearchUserResults)
    async user(
       @Arg("fieldValue") fieldValue: string,
       @Arg("fieldToSearch", type => FieldsToSearchUser) fieldToSearch: FieldsToSearchUser,
-   ): Promise<User> {
-      const findUserByIdUseCase = container.resolve(FindUserUseCase);
-      const user = await findUserByIdUseCase.execute(fieldToSearch, fieldValue);
-      return user
+      @Ctx() context: IContext
+
+   ): Promise<typeof SearchUserResults> {
+      const authorizationHeader = context.req.headers.authorization;
+
+      const findUserUseCase = container.resolve(FindUserUseCase);
+      const user = await findUserUseCase.execute(authorizationHeader, fieldToSearch, fieldValue);
+      return user;
    }
 
-   @Query(returns => [User])
-   async users(@Args() { skip, take }: usersArgs): Promise<User[]> {
+   @Query(returns => [SearchUsersResults])
+   async users(
+      @Args() { skip, take }: usersArgs
+   ): Promise<typeof SearchUsersResults[]> {
+      // 
+      // TODO: add pagination
+      // TODO: add error handling for everything
       const listUsersUseCase = container.resolve(ListUsersUseCase);;
       const users = await listUsersUseCase.execute();
       return users
    }
 
-   @FieldResolver()
-   async profile (@Root() user: User): Promise<Profile> {
+   @FieldResolver( returns => SearchUserProfileResults )
+   async profile (
+      @Root() user: User
+   ): Promise<typeof SearchUserProfileResults> {
       const findProfileByUserIdUseCase = container.resolve(FindProfileByUserIdUseCase);
       const profileFound = await findProfileByUserIdUseCase.execute( user.id );
-      if (!profileFound) {
-         throw new Error("Profile not found");
-      }
       return profileFound;
    }
 }
