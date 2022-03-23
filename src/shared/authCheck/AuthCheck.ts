a8a252b9f6ef70ebc7eca5631876b5968110bcc3
mikhael-dantas <mikhael-dcs@outlook.com>
2022-03-23 16:07:20 -0300

    implementing auth function

---
+++ src/shared/authCheck/AuthCheck.ts
@@ -0,0 +1,49 @@
+require('dotenv').config();
+import { Request } from "express";
+import { verify } from 'jsonwebtoken';
+import { ExpiredTokenErrorTypeDef } from "../errors/GraphqlErrorDefs/ExpiredTokenError";
+import { MissingTokenErrorTypeDef } from "../errors/GraphqlErrorDefs/MissingTokenError";
+
+interface IPayload {
+   user: {
+      id: string;
+      name: string;
+      email: string;
+      password: string;
+      created_at: string;
+   },
+   sub: string;
+}
+
+export function AuthCheck(
+   authorization: string | undefined
+   ) {
+   const authHeader = authorization
+
+   const secret = process.env.JWT_SECRET
+
+   if (!secret) {
+      console.log('JWT_SECRET is not defined in .env file');
+      throw new Error("Internal server error")
+   }
+
+   if (!authHeader) {
+      return new MissingTokenErrorTypeDef()
+   }
+
+   const [, token] = authHeader.split(" ");
+
+   try {
+      // const { sub: user_id } = verify(token, secret) as IPayload;
+      const decoded = verify(token, secret) as IPayload;
+      const { user } = decoded;
+
+      return {
+         user,
+         status: 200,
+         message: "user is authenticated",
+      };
+   } catch {
+      return new ExpiredTokenErrorTypeDef()
+   }
+}
