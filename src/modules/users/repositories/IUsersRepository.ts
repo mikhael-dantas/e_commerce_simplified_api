ca071b586e3a8e1132a256b419ce5a230ffe59d9
mikhael-dantas <mikhael-dcs@outlook.com>
2022-07-14 12:24:42 -0300

    removed code test folder

--- src/modules/users/repositories/IUsersRepository.ts
+++
@@ -1,9 +0,0 @@
-import { ICreateUserDTO, IFindAllDTO } from '../DTOs/UsersDTOs';
-import { User } from '../typeDefs/UserTypeDef'
-
-export interface IUsersRepository {
-   create: (data: ICreateUserDTO) => Promise<User>;
-   findAll: (data: IFindAllDTO) => Promise<User[]>;
-   findByEmail: (email: string) => Promise<User | null>;
-   findById: (user_id: string) => Promise<User | null>;
-}
