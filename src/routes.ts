a9ec515ee4d743f647560d80d07cc1e009d567ea
mikhael-dantas <mikhael-dcs@outlook.com>
2022-07-18 13:51:59 -0300

    removed auth imports temporally

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import { AuthUserController } from './modules/users/services/AuthController';
+// import { AuthUserController } from './modules/users/services/AuthController';
 
 const router = Router();
 
-const authUserController = new AuthUserController();
+// const authUserController = new AuthUserController();
 
-router.post('/auth', authUserController.create);
+// router.post('/auth', authUserController.create);
 
 export { router };
