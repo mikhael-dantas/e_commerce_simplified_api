aab22f67e260f5a9ae4639e6c092efc904bff447
mikhael-dantas <mikhael-dcs@outlook.com>
2022-07-24 14:41:10 -0300

    added infra tests to test db and env variables

--- __tests__/infra/infra.test.ts
+++ __tests__/infra/infra.test.ts
@@ -3,16 +3,54 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient()
-jest.setTimeout(30000)
+jest.setTimeout(25000)
 
 test("should be able to connect with database", async () => {
-   const connectAndDisconnect = async () => {
-      await prisma.$connect()
-      await new Promise(resolve => setTimeout(resolve, 10000))
-      await prisma.$disconnect()
-      await new Promise(resolve => setTimeout(resolve, 10000))
-      return true
-   }
+   await new Promise((resolve, reject) => {
+      prisma.$connect().then(() => {
+         resolve('done')
+      }).catch(err => {
+         reject(err)
+      })
+   }).catch(err => {
+      throw new Error("Error connecting to database")
+   })
+   
+   await new Promise((resolve, reject) => {
+      prisma.$disconnect().then(() => {
+         resolve('done')
+      }).catch(err => {
+         reject(err)
+      })
+   }).catch(err => {
+      throw new Error("Error disconnecting from database")
+   })
+})
 
-   expect(await connectAndDisconnect()).toBe(true)
+test("should have all the correct environment variables", async () => {
+   const env = require("dotenv").config()
+   const correctVariables = [
+      "SERVER_HOST",
+      "SERVER_PORT",
+      "DATABASE_URL",
+      "JWT_SECRET",
+      "JWT_EXPIRATION",
+      // "JWT_REFRESH_EXPIRATION",
+      // "JWT_REFRESH_SECRET",
+      // "JWT_REFRESH_ALGORITHM",
+      // "JWT_ALGORITHM",
+   ]
+   
+   const parsedKeys = Object.keys(env.parsed)
+   const parsedValues = Object.values(env.parsed)
+
+   expect(env).toHaveProperty("parsed")
+
+   correctVariables.forEach(key => {
+      expect(parsedKeys).toContain(key)
+   })
+
+   parsedValues.forEach(value => {
+      expect(value).toBeDefined()
+   })
 })
