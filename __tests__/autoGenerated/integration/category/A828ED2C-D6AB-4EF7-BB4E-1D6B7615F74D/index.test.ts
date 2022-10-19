beba866b60185210b4d7dd5574b33d5b59764b0d
mikhael dantas <mikhael-dcs@outlook.com>
2022-10-19 13:37:18 -0300

    list and create tests logic

--- __tests__/autoGenerated/integration/category/A828ED2C-D6AB-4EF7-BB4E-1D6B7615F74D/index.test.ts
+++ __tests__/autoGenerated/integration/category/A828ED2C-D6AB-4EF7-BB4E-1D6B7615F74D/index.test.ts
@@ -1,4 +1,3 @@
-
 // A828ED2C-D6AB-4EF7-BB4E-1D6B7615F74D
 // positionLabel5
 test(
@@ -6,7 +5,58 @@ test(
 // positionLabel6
 // positionLabel7
 
-() => {expect(true).toBe(false)}
+async () => {
+
+    const GET_CATEGORIES = `
+        query {
+            categories(take: 10, skip: 0) {
+                __typename
+                ...on Category {
+                    id,
+                    name,
+                    description,
+                    image_id,
+                    image_url,
+                    created_at,
+                    updated_at,
+                }
+            }
+        }
+    `
+
+    const query = {
+        query: GET_CATEGORIES,
+    }
+    
+    const {api__} = global as any
+    const {prismaClient} = global as any
+
+    prismaClient.category.create({
+        data: {
+            id: `${Math.random().toString()}${new Date().getTime()}`,
+            name: 'test',
+            description: 'test',
+            image_id: 'test',
+            image_url: 'test',
+            created_at: new Date(),
+            updated_at: new Date(),
+        }
+    })
+
+    const parsedRes = JSON.parse(await api__.post(JSON.stringify(query)))
+
+
+
+
+    expect(parsedRes.data?.categories).toBeDefined()
+    expect(parsedRes.data?.categories[0]).toHaveProperty('id')
+    expect(parsedRes.data?.categories[0]).toHaveProperty('name')
+    expect(parsedRes.data?.categories[0]).toHaveProperty('description')
+    expect(parsedRes.data?.categories[0]).toHaveProperty('image_path')
+    expect(parsedRes.data?.categories[0]).toHaveProperty('image_url')
+    expect(parsedRes.data?.categories[0]).toHaveProperty('created_at')
+    expect(parsedRes.data?.categories[0]).toHaveProperty('updated_at')
+}
 )
 // positionLabel8
 // positionLabel1-list all categories-positionLabel2
--- __tests__/autoGenerated/integration/category/F31793C7-CA2E-4B58-975E-3F1B2D42B8A8/index.test.ts
+++ __tests__/autoGenerated/integration/category/F31793C7-CA2E-4B58-975E-3F1B2D42B8A8/index.test.ts
@@ -1,4 +1,3 @@
-
 // F31793C7-CA2E-4B58-975E-3F1B2D42B8A8
 // positionLabel5
 test(
@@ -6,7 +5,47 @@ test(
 // positionLabel6
 // positionLabel7
 
-() => {expect(true).toBe(false)}
+async () => {
+    const CREATE_CATEGORY = `
+        mutation {
+            createCategory(
+                name: "test category",
+                description: "test description",
+                image_id: "test image id",
+                image_url: "test image url",
+            ) {
+                __typename
+                ...on Category {
+                    id,
+                    name,
+                    description,
+                    image_id,
+                    image_url,
+                    created_at,
+                    updated_at,
+                }
+            }
+        }
+    `
+
+    const query = {
+        query: CREATE_CATEGORY,
+    }
+
+    const {api__} = global as any
+    const parsedRes = JSON.parse(await api__.post(JSON.stringify(query)))
+
+    expect(parsedRes.data?.createCategory).toBeDefined()
+    expect(parsedRes.data?.createCategory).toHaveProperty('id')
+    expect(parsedRes.data?.createCategory).toHaveProperty('name')
+    expect(parsedRes.data?.createCategory).toHaveProperty('description')
+    expect(parsedRes.data?.createCategory).toHaveProperty('image_path')
+    expect(parsedRes.data?.createCategory).toHaveProperty('image_url')
+    expect(parsedRes.data?.createCategory).toHaveProperty('created_at')
+    expect(parsedRes.data?.createCategory).toHaveProperty('updated_at')
+}
+
+
 )
 // positionLabel8
 // positionLabel1-create a category-positionLabel2
