cfb435772ab14a3e5ff6a71e9f7de66075378d3d
mikhael dantas <mikhael-dcs@outlook.com>
2022-10-22 14:51:32 -0300

    pulled tests update

--- __tests__/autoGenerated/integration/category/F31793C7-CA2E-4B58-975E-3F1B2D42B8A8/nestedUseCases/nestedUseCases.test.ts
+++ __tests__/autoGenerated/integration/category/F31793C7-CA2E-4B58-975E-3F1B2D42B8A8/nestedUseCases/nestedUseCases.test.ts
@@ -1,1 +1,13 @@
-test.concurrent("no nested use cases", () => {expect(true).toBe(true)})
+
+// 9CA11B02-C481-46CC-B300-E25DB8FC37F9
+// positionLabel5
+test.concurrent(
+'should return defined error if any inputs are invalid ',
+// positionLabel6
+// positionLabel7
+
+() => {expect(true).toBe(false)}
+)
+// positionLabel8
+// positionLabel1-should return defined error if any inputs are invalid -positionLabel2
+// 9CA11B02-C481-46CC-B300-E25DB8FC37F9
---
+++ __tests__/autoGenerated/unit/category/14CDD5BA-056C-4CCC-BAA3-B4A0D3B5F822/index.test.ts
@@ -0,0 +1,16 @@
+
+// 14CDD5BA-056C-4CCC-BAA3-B4A0D3B5F822
+// positionLabel5
+test.concurrent(
+'usecase for create category must return defined error if name input is invalid by having more than 253 characters ',
+// positionLabel6
+// positionLabel7
+
+
+() => {
+;expect(true).toBe(false);
+}
+)
+// positionLabel8
+// positionLabel1-usecase for create category must return defined error if name input is invalid by having more than 253 characters -positionLabel2
+// 14CDD5BA-056C-4CCC-BAA3-B4A0D3B5F822
