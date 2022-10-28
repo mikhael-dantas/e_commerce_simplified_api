a169de6a91d341137cfa189df5bda385d38ecb6b
mikhael dantas <mikhael-dcs@outlook.com>
2022-10-28 13:23:11 -0300

    fixed host and dockerfile setup

--- dockerfile
+++ dockerfile
@@ -1,3 +1,4 @@
 FROM node:18.12
 WORKDIR /backend
+copy . .
 RUN corepack enable
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 require('dotenv').config();
 import { returnApp } from "./app";
 
-const port = process.env.SERVER_PORT ? process.env.SERVER_PORT : 3000
+const port = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 3000
+
+const host = process.env.SERVER_HOST ? process.env.SERVER_HOST : '0.0.0.0' 
 
 const serverInit = async () => {
    const app = await returnApp()
-   app.listen(port, () => {
-      console.log(`Server started on port ${port}`)
+   app.listen(port, host, () => {
+      console.log(`Server started on ${host}:${port}`)
    })
 }
 
