972fb8ea11b1c0a1e233e3fe23385a98af173f50
mikhael dantas <mikhael-dcs@outlook.com>
2023-02-23 18:04:06 -0300

    implemented test

---
+++ __tests__/autoGenerated/unit/user/5262665F-B2E4-42DF-95B3-FA379204EB08/index.test.ts
@@ -0,0 +1,34 @@
+import { createUserUseCase } from './../../../../../src/modules/users/useCases/CreateUser/index';
+
+
+// 5262665F-B2E4-42DF-95B3-FA379204EB08
+// positionLabel5
+test.concurrent(
+'use case for createUser(id: string)  should return the created user data, after creating it in the database',
+// positionLabel6
+// positionLabel7
+
+async () => {
+    const mockedUsersRepository: any = {
+        createUser: jest.fn().mockImplementation((data) => {
+            return {
+                id: data.id,
+            }
+        })
+    }
+
+    const useCase = new createUserUseCase(
+        mockedUsersRepository,
+    )
+
+    const response = await useCase.execute({
+        id: 'myid',
+    })
+
+    expect(response).toHaveProperty('id')
+    expect(response.id).toBe('myid')
+}
+)
+// positionLabel8
+// positionLabel1-use case for createUser(id: string)  should return the created user data, after creating it in the database-positionLabel2
+// 5262665F-B2E4-42DF-95B3-FA379204EB08
