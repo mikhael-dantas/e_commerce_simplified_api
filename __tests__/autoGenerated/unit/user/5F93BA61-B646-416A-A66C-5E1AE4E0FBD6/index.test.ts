4155357248fd79de4459fdb8d1f8ae45abb74dd6
mikhael dantas <mikhael-dcs@outlook.com>
2023-02-23 18:58:13 -0300

    implemented test

---
+++ __tests__/autoGenerated/unit/user/5F93BA61-B646-416A-A66C-5E1AE4E0FBD6/index.test.ts
@@ -0,0 +1,102 @@
+import { IUsersRepository } from '../../../../../src/modules/users/repositories/IUsersRepository';
+import { CreateLoginRegistryUseCase } from './../../../../../src/modules/users/useCases/CreateLoginRegistry/index';
+
+
+// 5F93BA61-B646-416A-A66C-5E1AE4E0FBD6
+// positionLabel5
+test.concurrent(
+'use case for createLoginRegistry(id:string) should create and return the login registry created for that user',
+// positionLabel6
+// positionLabel7
+
+async () => {
+    const mockedUsersRepository = {
+        createLoginRegistry: jest.fn().mockImplementation((data) => {
+            return {
+                id: '1',
+                created_at: new Date(),
+                user_id: data.userId,
+            }
+        }),
+        createUser: jest.fn().mockImplementation((data) => {
+            return {
+                id: data.id,
+            }
+        }),
+        findUserById: jest.fn().mockImplementation((data) => {
+            return {
+                id: data.id,
+            }
+        }),
+        listLoginRegistries: jest.fn().mockImplementation((data) => {
+            return [
+                {
+                    id: '1',
+                    created_at: new Date(),
+                    user_id: data.userId,
+                },
+                {
+                    id: '2',
+                    created_at: new Date(),
+                    user_id: data.userId,
+                },
+                {
+                    id: '3',
+                    created_at: new Date(),
+                    user_id: data.userId,
+                },
+            ]
+        })
+    }
+
+    const mockedCheckAcessTokenUseCase = {
+        execute: jest.fn().mockImplementation((data) => {
+            return {
+                id: '1',
+                sub: 'myid',
+            }
+        })
+    }
+
+    const mockedFindUserByIdUseCase = {
+        execute: jest.fn().mockImplementation((data) => {
+            return {
+                id: '1',
+            }
+        })
+    }
+
+    const mockedCreateUserUseCase = {
+        execute: jest.fn().mockImplementation((data) => {
+            return {
+                id: '1',
+            }
+        })
+    }
+
+
+
+    const useCase = new CreateLoginRegistryUseCase(
+        mockedUsersRepository as any,
+        mockedCheckAcessTokenUseCase as any,
+        mockedFindUserByIdUseCase as any,
+        mockedCreateUserUseCase as any,
+    )
+
+
+    const response = await useCase.execute({
+        accessToken: 'mytoken',
+        secret: 'mysecret',
+    })
+
+
+    expect(response).toHaveProperty('id')
+    expect(response.id).toBe('1')
+    expect(response).toHaveProperty('created_at')
+    expect(response).toHaveProperty('user_id')
+    expect(response.user_id).toBe('myid')
+}
+)
+// positionLabel8
+// positionLabel1-use case for createLoginRegistry(id:string) should create and return the login registry created for that user-positionLabel2
+// 5F93BA61-B646-416A-A66C-5E1AE4E0FBD6
