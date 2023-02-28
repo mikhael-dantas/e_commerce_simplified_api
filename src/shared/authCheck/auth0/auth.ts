d238a446a05735f8dddee709db1c3fb611f9106e
mikhael dantas <mikhael-dcs@outlook.com>
2023-02-28 14:09:34 -0300

    implemented get certificate from autho function

---
+++ src/shared/authCheck/auth0/auth.ts
@@ -0,0 +1,24 @@
+import { decode } from "jsonwebtoken";
+import JwksRsa from "jwks-rsa";
+
+export async function GetSecret(scopedAccessToken: string): Promise<string> {
+    const decoded = decode(scopedAccessToken, { complete: true })
+    if (!decoded) {
+        throw new Error("Invalid token")
+    }
+    const kid = decoded.header.kid;
+    const domain = process.env.DOMAIN
+    if (!domain) {
+        throw new Error("DOMAIN not defined")
+    }
+    const publicKeyFromCertificate = (await JwksRsa({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `https://${domain}/.well-known/jwks.json`
+    }).getSigningKey(kid)).getPublicKey()
+
+
+    console.log("publicKeyFromCertificate", publicKeyFromCertificate)
+    return publicKeyFromCertificate
+}
