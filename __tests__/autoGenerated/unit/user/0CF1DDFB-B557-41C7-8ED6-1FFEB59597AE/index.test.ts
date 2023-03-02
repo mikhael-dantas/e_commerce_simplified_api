da93bfaa2bf093eb34284a98f6bca5459da7b247
mikhael dantas <mikhael-dcs@outlook.com>
2023-03-02 12:01:40 -0300

    updated test name

--- __tests__/autoGenerated/unit/user/0CF1DDFB-B557-41C7-8ED6-1FFEB59597AE/index.test.ts
+++ __tests__/autoGenerated/unit/user/0CF1DDFB-B557-41C7-8ED6-1FFEB59597AE/index.test.ts
@@ -1,12 +1,12 @@
 import { UsersResolver } from './../../../../../src/modules/users/resolvers/index';
 
-
 // 0CF1DDFB-B557-41C7-8ED6-1FFEB59597AE
 // positionLabel5
 test.concurrent(
-'resolver mutation for loginRegistration(accessToken: string) should check if the token is valid, then if user exists, and if don\'t, create a user in the database with that sub id, then store the login registry, then returns a register of the last 2 logins from the user',
+`resolver mutation for loginRegistration(accessToken: string) should check if the token is valid, then if user exists, and if don't, create a user in the database with that sub id, then store the login registry, then returns a register of the login registry`,
 // positionLabel6
 // positionLabel7
+
 
 async () => {
     const mockedCreateRegistryUseCase = {
@@ -29,8 +29,9 @@ async () => {
     expect(loginRegistry.id).toBe('1')
     expect(loginRegistry.user_id).toBe('user_id1')
     expect(loginRegistry.created_at).toBeDefined()
+
 }
 )
 // positionLabel8
-// positionLabel1-resolver mutation for loginRegistration(accessToken: string) should check if the token is valid, then if user exists, and if don't, create a user in the database with that sub id, then store the login registry, then returns a register of the last 2 logins from the user-positionLabel2
+// positionLabel1-resolver mutation for loginRegistration(accessToken: string) should check if the token is valid, then if user exists, and if don't, create a user in the database with that sub id, then store the login registry, then returns a register of the login registry-positionLabel2
 // 0CF1DDFB-B557-41C7-8ED6-1FFEB59597AE
