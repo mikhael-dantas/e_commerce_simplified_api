38ecbf9bc5741cfb50670ae5918c930f5d786d3f
mikhael dantas <mikhael-dcs@outlook.com>
2023-03-05 11:13:51 -0300

    implemented test updates

--- __tests__/autoGenerated/integration/user/9508DE4A-A3AD-4641-99B2-D26A315E9B66/index.test.ts
+++ __tests__/autoGenerated/integration/user/9508DE4A-A3AD-4641-99B2-D26A315E9B66/index.test.ts
@@ -4,14 +4,18 @@ import { JestApiPost } from "../../../../../jestTestsUtils"
 // 9508DE4A-A3AD-4641-99B2-D26A315E9B66
 // positionLabel5
 test.concurrent(
-`must receive a mutation with (accessToken: string) parameter for loginRegistration, and after storing a login record in the database for the given user (that uses "sub" token payload as ID for the user table), recording the date of the login, but before it, check if user exists and if don't, create a user in the database with that id. then, returns a register of the last login from the user, and the user id`,
+`mutation for loginRegistration receiving accessToken and the client key, returns a register of the last login from the user, and the user id`,
 // positionLabel6
 // positionLabel7
+
 
 
 async () => {
     const secret = process.env.AUTH0_JWT_SECRET_TEST
     if (!secret) { throw new Error('No secret found in env') }
+    const clientKey = process.env.CLIENT_KEY
+    if (!clientKey) { throw new Error('No client key found in env') }
+
     const token = sign({
         sub: '1',
         name: 'John Doe',},
@@ -34,7 +38,10 @@ async () => {
         `
     }
 
-    const response = await JestApiPost(JSON.stringify(mutation))
+    const response = await JestApiPost(JSON.stringify(mutation), {headers: {
+        "Content-Type": "application/json",
+        'authorization': 'Bearer ' + clientKey
+    }})
 
     const parsedRes = JSON.parse(response)
 
@@ -44,8 +51,9 @@ async () => {
     expect(parsedRes.data?.loginRegistration).toHaveProperty('created_at')
     expect(parsedRes.data?.loginRegistration).toHaveProperty('user_id')
 
+
 }
 )
 // positionLabel8
-// positionLabel1-must receive a mutation with (accessToken: string) parameter for loginRegistration, and after storing a login record in the database for the given user (that uses "sub" token payload as ID for the user table), recording the date of the login, but before it, check if user exists and if don't, create a user in the database with that id. then, returns a register of the last login from the user, and the user id-positionLabel2
+// positionLabel1-mutation for loginRegistration receiving accessToken and the client key, returns a register of the last login from the user, and the user id-positionLabel2
 // 9508DE4A-A3AD-4641-99B2-D26A315E9B66
