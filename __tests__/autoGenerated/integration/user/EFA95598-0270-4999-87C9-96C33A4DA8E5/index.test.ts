134f351ad56f5aff9d965166e8e543cc71efd424
mikhael dantas <mikhael-dcs@outlook.com>
2023-03-05 11:15:41 -0300

    implemented test updates

--- __tests__/autoGenerated/integration/user/EFA95598-0270-4999-87C9-96C33A4DA8E5/index.test.ts
+++ __tests__/autoGenerated/integration/user/EFA95598-0270-4999-87C9-96C33A4DA8E5/index.test.ts
@@ -1,15 +1,17 @@
-
 import { JestApiPost } from "../../../../../jestTestsUtils"
 
 // EFA95598-0270-4999-87C9-96C33A4DA8E5
 // positionLabel5
 test.concurrent(
-'receive a query for loginAttempt and returns the state for the attempt, and the expiration time for it',
+`receive a query for loginAttempt passing clientKey in header authorization bearer, and returns the state for the attempt, and the expiration time for it`,
 // positionLabel6
 // positionLabel7
 
+
 async () => {
-    
+    const clientKey = process.env.CLIENT_KEY
+    if (!clientKey) { throw new Error('No client key found in env') }
+
     const GET_STATE = `
     query {
         loginAttemptInit {
@@ -27,8 +29,12 @@ async () => {
     const query = {
         query: GET_STATE,
     }
-    // const {prismaClient} = global as any
-    const parsedRes = JSON.parse(await JestApiPost(JSON.stringify(query)))
+    const parsedRes = JSON.parse(await JestApiPost(JSON.stringify(query), {
+        headers: {
+            "Content-Type": "application/json",
+            "authorization": "Bearer " + clientKey
+        }
+    }))
 
     expect(parsedRes).toHaveProperty('data')
     expect(parsedRes.data?.loginAttemptInit).toBeDefined()
@@ -38,5 +44,5 @@ async () => {
 }
 )
 // positionLabel8
-// positionLabel1-receive a query for loginAttempt and returns the state for the attempt, and the expiration time for it-positionLabel2
+// positionLabel1-receive a query for loginAttempt passing clientKey in header authorization bearer, and returns the state for the attempt, and the expiration time for it-positionLabel2
 // EFA95598-0270-4999-87C9-96C33A4DA8E5
