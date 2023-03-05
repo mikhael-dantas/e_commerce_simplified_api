f3b7565142e7ed7da3e8031e920d0a954e8e00c3
mikhael dantas <mikhael-dcs@outlook.com>
2023-03-05 15:37:30 -0300

    implemented test updates

--- __tests__/autoGenerated/unit/user/0CF1DDFB-B557-41C7-8ED6-1FFEB59597AE/index.test.ts
+++ __tests__/autoGenerated/unit/user/0CF1DDFB-B557-41C7-8ED6-1FFEB59597AE/index.test.ts
@@ -3,12 +3,17 @@ import { UsersResolver } from './../../../../../src/modules/users/resolvers/inde
 // 0CF1DDFB-B557-41C7-8ED6-1FFEB59597AE
 // positionLabel5
 test.concurrent(
-`resolver mutation for loginRegistration(accessToken: string) should check if the token is valid, then if user exists, and if don't, create a user in the database with that sub id, then store the login registry, then returns a register of the login registry`,
+`resolver mutation for loginRegistration after authorizing client key and the access token and storing a login record in the database for the given user (that uses "sub" token payload as ID for the user table), recording the date of the login, but before it, check if user exists and if don't, create a user in the database with that id, then store the login registry, then returns a register of the login registry`,
 // positionLabel6
 // positionLabel7
 
 
+
 async () => {
+    const clientKey = process.env.CLIENT_KEY
+    if (!clientKey) { throw new Error('No client key found in env') }
+
+
     const mockedCreateRegistryUseCase = {
         execute: jest.fn().mockImplementation((data) => {
             return {
@@ -23,15 +28,23 @@ async () => {
         createLoginRegistryUseCase: mockedCreateRegistryUseCase as any,
     })
 
-    const loginRegistry = await resolver.loginRegistration( 'mytoken' )
+    const loginRegistry = await resolver.loginRegistration({
+        req: {
+            headers: {
+                "Content-Type": "application/json",
+                "authorization": 'Bearer ' + clientKey
+            }
+        }
+    }, 'mytoken' )
 
     expect(loginRegistry).toBeDefined()
     expect(loginRegistry.id).toBe('1')
     expect(loginRegistry.user_id).toBe('user_id1')
     expect(loginRegistry.created_at).toBeDefined()
 
+
 }
 )
 // positionLabel8
-// positionLabel1-resolver mutation for loginRegistration(accessToken: string) should check if the token is valid, then if user exists, and if don't, create a user in the database with that sub id, then store the login registry, then returns a register of the login registry-positionLabel2
+// positionLabel1-resolver mutation for loginRegistration after authorizing client key and the access token and storing a login record in the database for the given user (that uses "sub" token payload as ID for the user table), recording the date of the login, but before it, check if user exists and if don't, create a user in the database with that id, then store the login registry, then returns a register of the login registry-positionLabel2
 // 0CF1DDFB-B557-41C7-8ED6-1FFEB59597AE
