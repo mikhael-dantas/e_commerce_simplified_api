66535a39a4bc916fd16ebf504a625023bbd38b81
mikhael dantas <mikhael-dcs@outlook.com>
2023-03-05 15:39:04 -0300

    implemented updated test

--- __tests__/autoGenerated/unit/user/862B90D4-63D3-4DAC-9630-4F6A323D15D7/index.test.ts
+++ __tests__/autoGenerated/unit/user/862B90D4-63D3-4DAC-9630-4F6A323D15D7/index.test.ts
@@ -1,14 +1,17 @@
 import { UsersResolver } from './../../../../../src/modules/users/resolvers';
 
-
 // 862B90D4-63D3-4DAC-9630-4F6A323D15D7
 // positionLabel5
 test.concurrent(
-'resolver for query loginAttempt should return a object with {state:string, expiration: string} property from the usecase',
+'resolver for query loginAttempt passing clientKey in header authorization bearer, should call Client authorization use case and then return a object with {state:string, expiration: string} property from the operation usecase after executing it',
 // positionLabel6
 // positionLabel7
 
+
 async () => {
+    const clientKey = process.env.CLIENT_KEY
+    if (!clientKey) { throw new Error('No client key found in env') }
+
     const mockedLoginAttemptInitUseCase = {
         execute: jest.fn().mockReturnValue({
             model: 'state',
@@ -19,14 +22,23 @@ async () => {
 
     const resolver = new UsersResolver({loginAttemptInitUseCase: mockedLoginAttemptInitUseCase as any})
 
-    const result = await resolver.loginAttemptInit()
+    const result = await resolver.loginAttemptInit(
+        {req: {
+            headers: {
+                "Content-Type": "application/json",
+                "authorization": "Bearer " + clientKey
+            }
+        }},
+    )
 
     expect(result).toBeDefined()
     expect(result.model).toBe('state')
     expect(result).toHaveProperty('state')
     expect(result).toHaveProperty('expiration')
-}
+
+// this code fail was put in here because this test name was eddited
+;expect(true).toBe(false);}
 )
 // positionLabel8
-// positionLabel1-resolver for query loginAttempt should return a object with {state:string, expiration: string} property from the usecase-positionLabel2
+// positionLabel1-resolver for query loginAttempt passing clientKey in header authorization bearer, should call Client authorization use case and then return a object with {state:string, expiration: string} property from the operation usecase after executing it-positionLabel2
 // 862B90D4-63D3-4DAC-9630-4F6A323D15D7
