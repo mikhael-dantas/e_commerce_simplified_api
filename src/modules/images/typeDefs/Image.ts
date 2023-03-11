1a3c1c54fc8ca30c0de36dd94a1cf00dac05d086
mikhael dantas <mikhael-dcs@outlook.com>
2023-03-11 14:19:43 -0300

    created image type

---
+++ src/modules/images/typeDefs/Image.ts
@@ -0,0 +1,28 @@
+import { Field, ObjectType } from "type-graphql";
+
+@ObjectType()
+export class Image {
+    @Field()
+    model: string = "image";
+
+    @Field()
+    id: string;
+
+    @Field()
+    created_at: Date;
+
+    @Field()
+    name: string;
+
+    @Field()
+    description: string;
+
+    @Field()
+    tags: string[];
+
+    @Field()
+    image_url: string;
+
+    @Field()
+    user_id: string;
+}
