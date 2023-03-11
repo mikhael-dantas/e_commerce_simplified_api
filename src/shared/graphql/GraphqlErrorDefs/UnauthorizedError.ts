6c183ce33996218ebed865afb2d4ac385696bf94
mikhael dantas <mikhael-dcs@outlook.com>
2023-03-11 15:00:41 -0300

    updated type constructor

--- src/shared/graphql/GraphqlErrorDefs/UnauthorizedError.ts
+++ src/shared/graphql/GraphqlErrorDefs/UnauthorizedError.ts
@@ -2,6 +2,15 @@ import { ObjectType, Field } from "type-graphql";
 
 @ObjectType()
 export class UnauthorizedError {
+   constructor(
+      message?: string
+   ) {
+      if (message) {
+         this.message = message
+      }
+   }
+
+
    @Field()
    model: string ="unauthorizedError";
 
