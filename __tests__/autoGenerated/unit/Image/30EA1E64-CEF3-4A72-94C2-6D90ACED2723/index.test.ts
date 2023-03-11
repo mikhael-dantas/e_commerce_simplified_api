e718a3222f6540d5012799480b2c312969c11e49
mikhael dantas <mikhael-dcs@outlook.com>
2023-03-11 13:50:44 -0300

    implemented test

---
+++ __tests__/autoGenerated/unit/Image/30EA1E64-CEF3-4A72-94C2-6D90ACED2723/index.test.ts
@@ -0,0 +1,38 @@
+import { ImagesRepository } from './../../../../../src/modules/images/repositories/index';
+import { CreateMockContext } from './../../../../../jestTestsUtils';
+
+
+// 30EA1E64-CEF3-4A72-94C2-6D90ACED2723
+// positionLabel5
+test.concurrent(
+'repo for Image.create receiving all image needed data should return the created image',
+// positionLabel6
+// positionLabel7
+
+async () => {
+
+    const mockedPrisma = CreateMockContext().prisma
+
+    const repo = new ImagesRepository(mockedPrisma)
+
+    const image = await repo.create({
+        name: 'name',
+        description: 'description',
+        tags: ['tag1', 'tag2'],
+        image_url: 'image_url',
+        userId: 'userId',
+    })
+
+    expect(image).toBeDefined()
+    expect(image).toHaveProperty('name')
+    expect(image).toHaveProperty('description')
+    expect(image).toHaveProperty('tags')
+    expect(image).toHaveProperty('image_url')
+    expect(image).toHaveProperty('userId')
+    expect(image).toHaveProperty('id')
+    expect(image).toHaveProperty('createdAt')
+}
+)
+// positionLabel8
+// positionLabel1-repo for Image.create receiving all image needed data should return the created image-positionLabel2
+// 30EA1E64-CEF3-4A72-94C2-6D90ACED2723
