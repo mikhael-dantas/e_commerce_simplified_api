aeafb03b9724a6f4ce72bd3a4596b421f03da635
mikhael dantas <mikhael-dcs@outlook.com>
2023-03-11 14:16:58 -0300

    create image use case interface

---
+++ src/modules/images/useCases/createImage/interface.ts
@@ -0,0 +1,25 @@
+import { UnauthorizedError } from "../../../../shared/graphql/GraphqlErrorDefs/UnauthorizedTokenError";
+import { CheckAccessTokenUseCase } from "../../../users/useCases/CheckAccessToken";
+import { FindUserByIdUseCase } from "../../../users/useCases/FindUserById";
+import { Image } from "../../typeDefs/Image";
+
+export interface ICreateImageUseCase {
+    execute({ 
+        data,
+        injections
+    }: TCreateImageUseCaseDTO): Promise<Image | UnauthorizedError>
+}
+
+export type TCreateImageUseCaseDTO = {
+    data: {
+        name: string,
+        description: string,
+        tags: string[],
+        image_url: string,
+        accessToken: string,
+    },
+    injections?: {
+        findUserById: FindUserByIdUseCase,
+        checkAccessToken: CheckAccessTokenUseCase
+    }
+}
