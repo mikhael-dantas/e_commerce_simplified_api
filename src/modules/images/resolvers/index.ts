d37b8961e1b33d8904831c04f98e276e897604a2
mikhael dantas <mikhael-dcs@outlook.com>
2023-03-11 14:23:06 -0300

    implemented resolver logic for create image

---
+++ src/modules/images/resolvers/index.ts
@@ -0,0 +1,44 @@
+import { Arg, Mutation } from "type-graphql";
+import { ICreateImageUseCase } from "../useCases/createImage/interface";
+import { createImageResults } from "./ResultTypes";
+import { container } from "tsyringe";
+import { CreateImageUseCase } from "../useCases/createImage";
+
+export class ImagesResolver {
+    constructor(
+        private injections?: {
+            createImageUseCase?: ICreateImageUseCase;
+        }
+    ) {}
+
+    @Mutation(returns => createImageResults)
+    async createImage(
+        @Arg('accessToken') accessToken: string,
+        @Arg('name') name: string,
+        @Arg('description') description: string,
+        @Arg('image_url') image_url: string,
+        @Arg('tags') tags: string[],
+    ): Promise<typeof createImageResults> {
+        let useCase
+        if (!this.injections?.createImageUseCase) {
+            useCase = container.resolve(CreateImageUseCase);
+        } else {
+            useCase = this.injections.createImageUseCase;
+        }
+
+        const createImageResponse = await useCase.execute({
+            data: {
+                name,
+                description,
+                image_url,
+                tags,
+                accessToken
+            },
+        });
+
+        return createImageResponse
+    }
+
+
+
+}
