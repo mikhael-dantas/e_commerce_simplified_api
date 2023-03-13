4e70d5272acbe42f70bc61230cca7973c361cc66
mikhael dantas <mikhael-dcs@outlook.com>
2023-03-13 10:35:02 -0300

    updated test

--- __tests__/autoGenerated/unit/user/40227634-4C02-44CB-80A1-41F4DB9CCD74/index.test.ts
+++ __tests__/autoGenerated/unit/user/40227634-4C02-44CB-80A1-41F4DB9CCD74/index.test.ts
@@ -1,15 +1,13 @@
 import { CheckAccessTokenUseCase } from './../../../../../src/modules/users/useCases/CheckAccessToken/index';
-
-
-// 40227634-4C02-44CB-80A1-41F4DB9CCD74
-
 import { sign } from "jsonwebtoken"
 
+// 40227634-4C02-44CB-80A1-41F4DB9CCD74
 // positionLabel5
 test.concurrent(
-'use case for checkAccessToken should check if the token is valid and return the decoded token if it is',
+`use case for checkAccessToken should check if the token is valid and return the decoded token if it is, and also check the given permissions in the token permissions key.`,
 // positionLabel6
 // positionLabel7
+
 
 async () => {
     const useCase = new CheckAccessTokenUseCase()
@@ -35,12 +33,15 @@ async () => {
         token: jwtToken,
         secret,
         algorithm: 'HS256',
+        permissions: ["manager"]
     })
 
     expect(response).toHaveProperty('sub')
     expect(response.sub).toBe(payload.sub)
+    expect(response).toHaveProperty('permissions')
+
 }
 )
 // positionLabel8
-// positionLabel1-use case for checkAccessToken should check if the token is valid and return the decoded token if it is-positionLabel2
+// positionLabel1-use case for checkAccessToken should check if the token is valid and return the decoded token if it is, and also check the given permissions in the token permissions key.-positionLabel2
 // 40227634-4C02-44CB-80A1-41F4DB9CCD74
