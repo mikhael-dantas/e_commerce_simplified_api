a7234d02c58849267dba928bb83071ea744a6f3c
mikhael dantas <mikhael-dcs@outlook.com>
2023-03-13 11:13:38 -0300

    implemented test update

--- __tests__/autoGenerated/unit/Image/99E71704-3108-43CF-9C25-D8C12280E13E/index.test.ts
+++ __tests__/autoGenerated/unit/Image/99E71704-3108-43CF-9C25-D8C12280E13E/index.test.ts
@@ -1,12 +1,12 @@
 import { ImagesResolver } from './../../../../../src/modules/images/resolvers/index';
 
-
 // 99E71704-3108-43CF-9C25-D8C12280E13E
 // positionLabel5
 test.concurrent(
-`resolver for mutation createImage receiving client key, user identification and the name:String,  description,  tags      Json array,  image_url    String. and returns the created image data of that user that don't failed the authorization, so the flow is call user authorization, call createImage usecase, and then return the created image data.`,
+`resolver for mutation createImage receiving client key, user identification and the name:String,  description,  tags      Json array,  image_url    String. and returns the created image data of that user that don't failed the authorization, so the flow is call user authorization, call createImage usecase, and then return the created image data. And if authorization fails and user don't have manager in permissions token payload, return unauthorizedError.`,
 // positionLabel6
 // positionLabel7
+
 
 
 async () => {
@@ -24,7 +24,7 @@ async () => {
         })
     }
 
-    const resolver = new ImagesResolver({
+    let resolver = new ImagesResolver({
         createImageUseCase: mockedUseCase as any,
     })
 
@@ -44,8 +44,30 @@ async () => {
     expect(image).toHaveProperty('image_url')
     expect(image).toHaveProperty('created_at')
     expect(image).toHaveProperty('updated_at')
+
+    mockedUseCase.execute = jest.fn().mockImplementation(async (data) => {
+        throw new Error('unauthorized permissions')
+    })
+
+    resolver = new ImagesResolver({
+        createImageUseCase: mockedUseCase as any,
+    })
+
+    const image2 = await resolver.createImage(
+        "token",
+        "name",
+        "description",
+        "image_url",
+        ["tag1", "tag2"],
+    )
+
+    expect(image2).toBeDefined()
+    expect(image2).toHaveProperty('model')
+    expect(image2.model).toBe('unauthorizedError')
+    expect(image2).toHaveProperty('message')
+
 }
 )
 // positionLabel8
-// positionLabel1-resolver for mutation createImage receiving client key, user identification and the name:String,  description,  tags      Json array,  image_url    String. and returns the created image data of that user that don't failed the authorization, so the flow is call user authorization, call createImage usecase, and then return the created image data.-positionLabel2
+// positionLabel1-resolver for mutation createImage receiving client key, user identification and the name:String,  description,  tags      Json array,  image_url    String. and returns the created image data of that user that don't failed the authorization, so the flow is call user authorization, call createImage usecase, and then return the created image data. And if authorization fails and user don't have manager in permissions token payload, return unauthorizedError.-positionLabel2
 // 99E71704-3108-43CF-9C25-D8C12280E13E
