0f7f7c6eb798b4144af2a0395a87e5330079bcbd
mikhael dantas <mikhael-dcs@outlook.com>
2023-03-14 16:22:00 -0300

    implemented getimages resolver

--- src/modules/images/resolvers/index.ts
+++ src/modules/images/resolvers/index.ts
@@ -1,13 +1,16 @@
-import { Arg, Mutation } from "type-graphql";
+import { Arg, Int, Mutation, Query } from "type-graphql";
 import { ICreateImageUseCase } from "../useCases/createImage/interface";
-import { createImageResults } from "./ResultTypes";
+import { createImageResults, getImagesResults } from "./ResultTypes";
 import { container } from "tsyringe";
 import { CreateImageUseCase } from "../useCases/createImage";
+import { IListImagesUseCase } from "../useCases/listImages/interface";
+import { ListImagesUseCase } from "../useCases/listImages";
 
 export class ImagesResolver {
     constructor(
         private injections?: {
             createImageUseCase?: ICreateImageUseCase;
+            listImagesUseCase?: IListImagesUseCase;
         }
     ) {}
 
@@ -39,6 +42,38 @@ export class ImagesResolver {
         return createImageResponse
     }
 
+    @Query(returns => getImagesResults)
+    async getImages(
+        @Arg('skip', () => Int, { nullable: true }) skip?: number,
+        @Arg('take', () => Int, { nullable: true }) take?: number,
+        @Arg('orderBy', () => String, { nullable: true }) orderBy?: string,
+        @Arg('orderDirection', () => String, { nullable: true }) orderDirection?: string,
+        @Arg('name', () => String, { nullable: true }) name?: string,
+        @Arg('description', () => String, { nullable: true }) description?: string,
+        @Arg('tags', () => [String], { nullable: true }) tags?: string[],
+        @Arg('user_id', () => String, { nullable: true }) user_id?: string,
+        @Arg('image_url', () => String, { nullable: true }) image_url?: string,
+    ): Promise<typeof getImagesResults> {
+        let useCase
+        if (!this.injections?.listImagesUseCase) {useCase = container.resolve(ListImagesUseCase);
+        } else { useCase = this.injections.listImagesUseCase; }
 
+
+        const listImagesResponse = await useCase.execute({
+            data: {
+                skip,
+                take,
+                orderBy,
+                orderDirection,
+                name,
+                description,
+                tags,
+                user_id,
+                image_url
+            },
+        });
+
+        return listImagesResponse
+    }
 
 }
